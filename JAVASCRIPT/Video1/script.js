b8e929cb2297d56df6f5fb5d88051cb2888d91a1
Black-Magic-dark <rkumari.info1234@gmail.com>
2025-04-15 18:44:34 +0530

    C_S_S

---
+++ JAVASCRIPT/Video1/script.js
@@ -0,0 +1,15 @@
+
+/*alert("hii Magic");*/
+console.log("code is running..")
+console.log("code is also run..")
+console.log("code is looking like wow")
+ var a = prompt("enter the no.")
+ var isTrue =confirm("Do you want tp leave the page or blast your computer")
+  if(isTrue){
+    console.log("computer is blasting")
+  }
+  else{
+    console.log("computer is not blasting")
+  }
+  console.log("the no. is"+a)
+  document.title = "Hey I am good"
